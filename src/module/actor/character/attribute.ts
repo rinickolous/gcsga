3eb898d0230a9e802db24375d707ce1e77c137b9
neck <43256580+rinickolous@users.noreply.github.com>
2022-06-15 00:13:27 +0100

    started work on attribute defs

---
+++ src/module/actor/character/attribute.ts
@@ -0,0 +1,48 @@
+import { VariableResolver } from "@module/variable_resolver";
+
+export class Attribute {
+	id: string;
+	type: AttributeType;
+	name: string;
+	full_name: string;
+	attribute_base: string;
+	cost_per_point: number;
+	cost_adj_percent_per_sm: number;
+	thresholds: PoolThreshold[];
+	order: number;
+
+	constructor(data: Attribute) {
+		this.id = data.id;
+		this.type = data.type;
+		this.name = data.name;
+		this.full_name = data.full_name;
+		this.attribute_base = data.attribute_base;
+		this.cost_per_point = data.cost_per_point;
+		this.cost_adj_percent_per_sm = data.cost_adj_percent_per_sm;
+		this.thresholds = data.thresholds;
+		this.order = data.order;
+	}
+
+	get priamry() {
+		//@ts-ignore
+		return this.attribute_base == parseFloat(this.attribute_base);
+	}
+
+	get BaseValue() {
+		const vr = new VariableResolver();
+		return vr.evaluateToNumber(this.attribute_base);
+	}
+}
+
+export type AttributeType = "integer" | "decimal" | "pool";
+
+export type PoolThresholdOp = "halve_move" | "halve_dodge" | "halve_st";
+
+export interface PoolThreshold {
+	state: string;
+	explanation: string;
+	multiplier: number;
+	divisor: number;
+	addition: number;
+	ops: PoolThresholdOp[];
+}
